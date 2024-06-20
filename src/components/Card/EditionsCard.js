9c2d3633fae8ca4fba265df8398c90cf4a986c90
Sahil <sahilverma1931@gmail.com>
2024-06-20 08:34:10 +0530

    change text color of button-- to black

--- src/components/Card/EditionsCard.js
+++ src/components/Card/EditionsCard.js
@@ -81,8 +81,8 @@ const EditionsCard = () => {
               <img src={data.imageSrc} alt="" />
             </div>
             <h1>SRIJAN {data.year}</h1>
-            <Button className="button--big">
-              <a style={{ textDecoration: "none" }} href={data.link} target="_blank">
+            <Button className="button--big" >
+              <a style={{ textDecoration: "none" , color : "black"}} href={data.link} target="_blank">
                 Read Now
               </a>
             </Button>
